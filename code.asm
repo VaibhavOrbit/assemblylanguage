8b83b3523ef7041a58aa560218e420cb8896f89c
VaibhavOrbit <vaibhav.readme@gmail.com>
2025-11-13 04:31:58 +0000

    add comments

--- code.asm
+++ code.asm
@@ -1,21 +1,21 @@
-Program:
-	Data SEGMENT
-	A Dw  1234H
-	b DW  4231h
+
+Data SEGMENT 	 //Starts a segment by the name Data
+	A Dw  1234H 	//Declares A as 16bit with value 1234H
+	b DW  4231H  //Declares B as 16bits  with value 4321H
 	sum Dw  ? 
-	carry db  00h
+	carry db  00H 
 
 Data Ends
  
 code SEGMENT
-ASSUME CS:Code, DS: Data
-Start : MOV AX, Data
-	MOV DS, AX
-	MOV AX, A
-	ADD AX, B
-	JNC Skip
-	MOV Carry, 01H
-	skip MOV SUM, AX
-	INT3
+ASSUME CS:Code, DS: Data  // Informs the assembler about the correct segments 
+Start : MOV AX, Data       // Puts segment address of Data into AX
+	MOV DS, AX			// Transfer segment address of Data from AX to DS
+	MOV AX, A			// Gets the value of A into AX
+	ADD AX, B			// Adds the value of B into AX 
+	JNC Skip			// if no carry then make variable "carry=1"
+	MOV Carry, 01H       //  if carry produced then make variable "carry=1"
+	skip MOV SUM, AX     //  store the sum in the variable "Sum"
+	INT3				// Optional BReakpoint
 	Code Ends
 	End Start
